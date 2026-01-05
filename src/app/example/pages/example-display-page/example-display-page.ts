39b130a717d53e67ed53f349c4f58fe304aac3ec
Magic <magicpitchayapak@gmail.com>
2026-01-05 17:21:38 +0700

    test

---
+++ src/app/example/pages/example-display-page/example-display-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example-display-page',
+  imports: [],
+  templateUrl: './example-display-page.html',
+  styleUrl: './example-display-page.scss',
+})
+export class ExampleDisplayPage {
+
+}
